db118ea07aa340ae1d80d540f3ae923ff61712ab
ProgrammerIosif <programmeriosif@protonmail.com>
2023-02-12 19:54:04 +0200

    configured homepage path and hashrouter

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 
 const RouteSwitch = () => {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
-        <Route path="/shop/" element={<Home />} />
-        <Route path="/shop/shop" element={<Shop />} />
-        <Route path="/shop/cart" element={<Cart />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 };
 
