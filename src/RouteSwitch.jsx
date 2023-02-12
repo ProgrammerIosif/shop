d18713235a521ae91921c679385bb97809d10608
ProgrammerIosif <programmeriosif@protonmail.com>
2023-02-12 17:07:30 +0200

    switched browserRouter for HashRouter

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/shop/" element={<Home />} />
         <Route path="/shop/shop" element={<Shop />} />
         <Route path="/shop/cart" element={<Cart />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
