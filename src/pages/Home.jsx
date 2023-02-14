58308c5712b69d87543ba18000034c7967bb411c
ProgrammerIosif <programmeriosif@protonmail.com>
2023-02-14 21:11:16 +0200

    this is the shop where you build your tech-stack

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,11 @@ import Header from '../components/Header'
 
 function Home() {
   return (
-    <div className="h-full bg-cover bg-center bg-[url('https://amielfilarca.me/shopping-cart/static/media/rtx-3090.3aed8e8a.jpg')]" id='home'>
+    <div className=" h-[100vh] bg-fixed bg-cover bg-center bg-[url('https://amielfilarca.me/shopping-cart/static/media/rtx-3090.3aed8e8a.jpg')]" id='home'>
       <Header currPage='home'/>
       <div className="flex flex-col pt-[20vh] md:pt-[35vh] px-20">
-        <h1 className="text-5xl font-bold italic py-6"><a href="/shop">BUILD THE COMPUTER OF THE FUTURE</a></h1>
-        <p className="text-xl w-[70vw] md:w-[700px]">Shop here to assemble the ultimate cutting-edge computer, optimized for tomorrow's technology and your performance needs.</p>
+        <h1 className="text-5xl font-bold italic py-6"><a href="/shop">BUILD THE WEBAPP OF THE FUTURE</a></h1>
+        <p className="text-xl w-[70vw] md:w-[700px]">Shop here to assemble the ultimate cutting-edge teck-stack, optimized for tomorrow's expectations and your performance needs.</p>
       </div>
     </div>
   )
