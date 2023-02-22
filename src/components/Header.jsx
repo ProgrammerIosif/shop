3f3ee92d9deb0f465a7d16fe6b61c3bb13d5bd06
ProgrammerIosif <programmeriosif@protonmail.com>
2023-02-22 21:16:55 +0200

    mobile dropdown menu can be toggled off

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,19 @@
+import {useState} from "react";
+
 const Header = ({currPage}) => {
+  const [menuDisplay, setmenuDisplay] = useState(true);
+  const [displayMenuStyle, setdisplayMenuStyle] = useState('');
+
+  const showMenu = () => {
+    setmenuDisplay(!menuDisplay)
+    if (menuDisplay) {
+      setdisplayMenuStyle('')
+    } else {
+      setdisplayMenuStyle('none')
+    }
+    return displayMenuStyle;
+  }
+  
   return (
     <div id="header" className="bg-opacity-40 w-full h-24 drop-shadow-lg">
         <div className="container px-4 md:px-4 h-full mx-auto flex justify-between items-center">
@@ -27,10 +42,10 @@ const Header = ({currPage}) => {
 
           <div className="flex items-center md:hidden">
             <label className="btn btn-circle swap swap-rotate dropdown dropdown-bottom dropdown-end">
-              <input type="checkbox" />
+              <input type="checkbox" onClick={showMenu}/>
               <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
               <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
-              <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+              <ul style={{ display: displayMenuStyle}} tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
                 <li><a href="/shop">HOME</a></li> 
                 <li><a href="/shop/#/shop">SHOP</a></li>
                 <li><a href="/shop/#/cart">CART</a></li>
